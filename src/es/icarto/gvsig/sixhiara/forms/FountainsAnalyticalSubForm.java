604f24cbfdc43f421aef811be11990658159c9f4
psanxiao <psanxiao@icarto.es>
2013-09-19 12:10:45 +0200

    Updated abeille forms and refactor analytical table name

--- src/es/icarto/gvsig/sixhiara/forms/FountainsAnalyticalSubForm.java
+++ src/es/icarto/gvsig/sixhiara/forms/FountainsAnalyticalSubForm.java
@@ -5,7 +5,7 @@ import es.icarto.gvsig.navtableforms.gui.tables.AbstractSubForm;
 @SuppressWarnings("serial")
 public class FountainsAnalyticalSubForm extends AbstractSubForm {
 
-    public static final String TABLENAME = "analiticas";
+    public static final String TABLENAME = "analise";
     public static String[] colNames = { "cod_fonte", "data_most", "ph",
 	    "oxigeno_d", "coli_tot" };
     public static String[] colAlias = { "Cod Fonte", "Fecha Muestra", "PH",
