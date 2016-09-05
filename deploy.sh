fc6c8751249ecdcbbfa6cad8040f75c13dd7179e
Francisco Puga <fpuga@icarto.es>
2016-09-05 21:15:03 +0200

    Bugfixing

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ EXT=/home/fpuga/development/gvsig-desktop2.2.0-2312/org.gvsig.desktop-2.0.100/ta
 cp -r /var/tmp/gvSIG-desktop-2.3.0-2441-RC2-win-x86_64/ /tmp/${VERSION}
 
 rm -rf /tmp/${VERSION}/install/*
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/CSVWizard
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.annotation.app.mainplugin/
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.catalog.extension/
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.gazetteer.extension/
@@ -22,7 +23,6 @@ rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.scripting.app.extension/
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.symbology.app.symbolinstaller/
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.symbology.app.importsymbols/
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.attributeeditor.app.mainplugin/
-rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.daltransform.app.mainplugin/
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/AutodeteccionDeAlturas
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.customize.app.mainplugin
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.busquedacatastral.app.mainplugin
@@ -36,8 +36,24 @@ rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.timesupport.app.animation
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.timesupport.app.viewfilter
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/ScriptingComposerTools
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.wfs.app.mainplugin
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.raster.wmts.app.wmtsclient
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.raster.wms.app.wmsclient
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.raster.postgis.app.postgisrasterclient
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.raster.georeferencing.app.georeferencingclient
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.hyperlink.app.extension
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.googlemaps.app.streetview
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.geoprocess.app.sextante
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.geoprocess.app.mainplugin
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.geoprocess.app.algorithm
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.derivedgeometries.app.mainplugin
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.dgn.app.mainplugin
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.dwg.app.mainplugin
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.dxf.app.mainplugin
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.scripting.app.mainplugin
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.raster.principalcomponents.app.principalcomponentsclient
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.raster.roimask.app.client
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.raster.tasseledcap.app.tasseledcapclient
 # rm -rf /tmp/${VERSION}/gvSIG/extensiones/
-
 
 rm -rf /tmp/${VERSION}/home/gvSIG/plugins/org.gvsig.app.mainplugin/Symbols/
 
--- src/main/java/es/icarto/gvsig/sixhiara/LoadDefaultViewExtension.java
+++ src/main/java/es/icarto/gvsig/sixhiara/LoadDefaultViewExtension.java
@@ -4,6 +4,15 @@ import static es.icarto.gvsig.sixhiara.ConfigExtension.NAME;
 
 import org.gvsig.andami.PluginServices;
 import org.gvsig.app.project.documents.view.gui.IView;
+import org.gvsig.fmap.dal.DataStoreParameters;
+import org.gvsig.fmap.dal.coverage.RasterLocator;
+import org.gvsig.fmap.dal.coverage.RasterManager;
+import org.gvsig.fmap.dal.coverage.datastruct.Params;
+import org.gvsig.fmap.dal.coverage.store.RasterDataStore;
+import org.gvsig.fmap.mapcontext.MapContextLocator;
+import org.gvsig.fmap.mapcontext.MapContextManager;
+import org.gvsig.fmap.mapcontext.layers.FLayer;
+import org.gvsig.fmap.mapcontext.layers.FLayers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,14 +33,37 @@ public class LoadDefaultViewExtension extends AbstractExtension {
 		PluginServices.getMDIManager().setWaitCursor();
 		try {
 			IView view = Andami.createViewIfNeeded(NAME, "EPSG:32737");
-			MapDAO mapDAO = MapDAO.getInstance();
-			ELLEMap map = mapDAO.getMap(view, NAME, LoadLegend.DB_LEGEND, NAME);
-			map.load(view.getMapControl().getProjection());
+			loadVectorial(view);
+			// loadRaster(view);
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 		} finally {
 			PluginServices.getMDIManager().restoreCursor();
 		}
+	}
+
+	private void loadVectorial(IView view) throws Exception {
+		MapDAO mapDAO = MapDAO.getInstance();
+		ELLEMap map = mapDAO.getMap(view, NAME, LoadLegend.DB_LEGEND, NAME);
+		map.load(view.getMapControl().getProjection());
+
+	}
+
+	private void loadRaster(IView view) throws Exception {
+		String layerName = "";
+		FLayers layers = view.getMapControl().getMapContext().getLayers();
+		FLayers layerGroup = (FLayers) layers.getLayer(layerName);
+		RasterManager manager = RasterLocator.getManager();
+		RasterDataStore store = null;
+		// store = manager.getProviderServices().open("/tmp/PONTEVEDRA29.TIF");
+		Params param = manager.createParams("Gdal_Store", null, 0,
+				new String[] { "/tmp/PONTEVEDRA29.TIF" });
+		store = manager.getProviderServices().open((DataStoreParameters) param);
+		MapContextManager mapContextManager = MapContextLocator
+				.getMapContextManager();
+		FLayer layer = mapContextManager.createLayer("mi nombre", store);
+		layers.addLayer(layer);
+		// layerGroup.addLayer(layer, where, adjoiningLayer);
 	}
 
 	@Override
