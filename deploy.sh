a1c8feb8a0121229a0a0feb2e48f2ad72966d6b8
Francisco Puga <fpuga@icarto.es>
2016-11-08 20:48:12 +0100

    Adds a new extension to load gpx files

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,34 @@
 
 VERSION=`date +%g%m%d`_SIRHAN_Inventario
 
-EXT=/home/fpuga/development/gvsig-desktop2.2.0-2312/org.gvsig.desktop-2.0.100/target/product/gvSIG/extensiones
-# EXT=/home/fpuga/development/gvsig-desktop2.3.0RC4-2444/org.gvsig.desktop-2.0.151/target/product/gvSIG/extensiones
+EXT=/home/fpuga/development/gvsig-desktop2.3.1-2501/org.gvsig.desktop-2.0.157/target/product/gvSIG/extensiones/
+
 # unzip /var/tmp/gvsig-desktop-2.2.0-2313-final-win-x86.zip -d /tmp
-cp -r /var/tmp/gvsig-desktop-2.2.0-2313-final-win-x86/ /tmp/${VERSION}
-# cp -r /var/tmp/gvSIG-desktop-2.3.0-2444-RC4-win-x86_64/ /tmp/${VERSION}
+cp -r /var/tmp/gvSIG-desktop-2.3.1-2501-final-win-x86_64/ /tmp/${VERSION}
 
 
 rm -rf /tmp/${VERSION}/install/*
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/CSVWizard
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.animation3d.app
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.annotation.app.mainplugin/
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.busquedacatastral.app.mainplugin
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.catalog.extension/
-rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.gazetteer.extension/
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.chart.app.mainplugin
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.chart.app.layoutplugin
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.chart.app.legendplugin
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.derivedgeometries.app.mainplugin
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.dgn.app.mainplugin
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.dwg.app.mainplugin
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.dxf.app.mainplugin
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.dyschromatopsia.app.mainplugin/
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.gazetteer.extension/
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.geoprocess.app.sextante
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.geoprocess.app.mainplugin
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.geoprocess.app.algorithm
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.hyperlink.app.extension
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.lidar.app.mainplugin
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.lrs.app.mainplugin
+rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.r.app.mainplugin
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.publish.app.mainplugin/
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.raster.netcdf.app.netcdfclient/
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.seismic.app.mainplugin/
@@ -27,12 +42,6 @@ rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.symbology.app.importsymbols/
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.attributeeditor.app.mainplugin/
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/AutodeteccionDeAlturas
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.customize.app.mainplugin
-rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.busquedacatastral.app.mainplugin
-rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.chart.app.mainplugin
-rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.chart.app.layoutplugin
-rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.chart.app.legendplugin
-rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.lidar.app.mainplugin
-rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.lrs.app.mainplugin
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.raster.wcs.app.wcsclient
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.timesupport.app.animation
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.timesupport.app.viewfilter
@@ -42,23 +51,13 @@ rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.raster.wmts.app.wmtsclient
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.raster.wms.app.wmsclient
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.raster.postgis.app.postgisrasterclient
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.raster.georeferencing.app.georeferencingclient
-rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.hyperlink.app.extension
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.googlemaps.app.streetview
-rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.geoprocess.app.sextante
-rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.geoprocess.app.mainplugin
-rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.geoprocess.app.algorithm
-rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.derivedgeometries.app.mainplugin
-rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.dgn.app.mainplugin
-rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.dwg.app.mainplugin
-rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.dxf.app.mainplugin
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.scripting.app.mainplugin
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.raster.principalcomponents.app.principalcomponentsclient
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.raster.roimask.app.client
 rm -rf /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.raster.tasseledcap.app.tasseledcapclient
-# rm -rf /tmp/${VERSION}/gvSIG/extensiones/
 
 rm -rf /tmp/${VERSION}/home/gvSIG/plugins/org.gvsig.app.mainplugin/Symbols/
-
 
 cp -R ${EXT}/es.icarto.gvsig.commons /tmp/${VERSION}/gvSIG/extensiones/
 cp -R ${EXT}/es.icarto.gvsig.copyfeatures /tmp/${VERSION}/gvSIG/extensiones/
@@ -73,7 +72,15 @@ cp -R portable/gvSIG /tmp/${VERSION}/
 
 cp -R portable/i18n/ /tmp/${VERSION}/
 
-mv /tmp/${VERSION}/gvsig-desktop.exe /tmp/${VERSION}/SIRHAN_Inventario.exe
+# mv /tmp/${VERSION}/gvsig-desktop.exe /tmp/${VERSION}/SIRHAN_Inventario.exe
+mv /tmp/${VERSION}/gvsig-desktop.cmd /tmp/${VERSION}/SIRHAN_Inventario.cmd
+mv /tmp/${VERSION}/gvsig-desktop.vbs /tmp/${VERSION}/SIRHAN_Inventario.vbs
+
+### Hacked libs. Remove when fixed upstream
+cp ${EXT}/org.gvsig.app.mainplugin/lib/org.gvsig.fmap.dal.impl-2.0.157.jar /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.app.mainplugin/lib/org.gvsig.fmap.dal.impl-2.0.157.jar
+rm /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.gdal.app.ogr.mainplugin/lib/org.gvsig.gdal.prov.ogr-1.0.30.jar
+cp ${EXT}/org.gvsig.gdal.app.ogr.mainplugin/lib/org.gvsig.gdal.prov.ogr-1.0.32.jar /tmp/${VERSION}/gvSIG/extensiones/org.gvsig.gdal.app.ogr.mainplugin/lib/org.gvsig.gdal.prov.ogr-1.0.32.jar
+
 # cd /tmp
 # zip -r ${VERSION} /tmp/${VERSION}
 
---
+++ src/main/java/es/icarto/gvsig/sixhiara/LoadGPXExtension.java
@@ -0,0 +1,62 @@
+package es.icarto.gvsig.sixhiara;
+
+import static es.icarto.gvsig.commons.i18n.I18n._;
+
+import java.awt.Component;
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import org.gvsig.andami.PluginServices;
+import org.gvsig.app.project.documents.view.gui.IView;
+import org.gvsig.fmap.mapcontext.exceptions.LoadLayerException;
+import org.gvsig.fmap.mapcontext.layers.FLayers;
+import org.gvsig.fmap.mapcontext.layers.vectorial.FLyrVect;
+import org.gvsig.gui.beans.swing.JFileChooser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import es.icarto.gvsig.commons.AbstractExtension;
+import es.icarto.gvsig.commons.datasources.GPXFactory;
+
+public class LoadGPXExtension extends AbstractExtension {
+	
+	private static final Logger logger = LoggerFactory
+			.getLogger(LoadGPXExtension.class);
+
+	private IView view;
+
+	@Override
+	public void execute(String actionCommand) {
+		String homeFolder = System.getProperty("user.home");
+		JFileChooser fileChooser = new JFileChooser(getClass().getName(), homeFolder);
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		FileFilter gpxFilter = new FileNameExtensionFilter(_("gps_files"), "gpx");
+		fileChooser.setFileFilter(gpxFilter);
+		Component parent = (Component) PluginServices.getMainFrame();
+		if (fileChooser.showDialog(parent, _("load"))  == JFileChooser.APPROVE_OPTION) {
+			File selectedFile = fileChooser.getSelectedFile();
+			if (fileChooser.accept(selectedFile)) {
+				loadGPX(selectedFile);
+			}
+		}
+	}
+	
+	private void loadGPX(File selectedFile) {
+		try {
+			FLyrVect f = GPXFactory.getWaypointLyrFromGPX(selectedFile, "EPSG:4326");
+			FLayers layers = view.getMapControl().getMapContext().getLayers();
+			layers.add(f);
+		} catch (LoadLayerException e) {
+			logger.error(e.getMessage(), e);
+		}
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		view = getView();
+		return view != null;
+	}
+
+}
