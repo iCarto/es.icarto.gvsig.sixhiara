fb76c190cff468a2a0d5ee520c11dc1b09102a0c
Francisco Puga <fpuga@icarto.es>
2019-07-08 21:48:02 +0200

    Bugfixing.
    * Fixes cosmetic issues in abeille forms
    * Fixes bad column name for carac_hidro. It was making that font form does not work
    
    refs #1689

--- apply_patches.sh
+++ apply_patches.sh
@@ -11,9 +11,9 @@ fi
 
 ### Hacked libs. Remove when fixed upstream
 cp portable/common/patches/org.gvsig.fmap.dal.impl-2.0.157.jar ${BASE_PATCHES_PATH}/gvSIG/extensiones/org.gvsig.app.mainplugin/lib/org.gvsig.fmap.dal.impl-2.0.157.jar
-rm ${BASE_PATCHES_PATH}/gvSIG/extensiones/org.gvsig.gdal.app.ogr.mainplugin/lib/org.gvsig.gdal.prov.ogr-1.0.30.jar
+rm -f ${BASE_PATCHES_PATH}/gvSIG/extensiones/org.gvsig.gdal.app.ogr.mainplugin/lib/org.gvsig.gdal.prov.ogr-1.0.30.jar
 cp portable/common/patches/org.gvsig.gdal.prov.ogr-1.0.32.jar ${BASE_PATCHES_PATH}/gvSIG/extensiones/org.gvsig.gdal.app.ogr.mainplugin/lib/org.gvsig.gdal.prov.ogr-1.0.32.jar
 cp portable/common/patches/org.gvsig.datalocator.app.mainplugin-2.0.157.jar ${BASE_PATCHES_PATH}/gvSIG/extensiones/org.gvsig.datalocator.app.mainplugin/lib/org.gvsig.datalocator.app.mainplugin-2.0.157.jar
 cp portable/common/patches/org.gvsig.fmap.dal.db.jdbc-2.0.157.jar ${BASE_PATCHES_PATH}/gvSIG/extensiones/org.gvsig.app.mainplugin/lib/
-rm ${BASE_PATCHES_PATH}/gvSIG/extensiones/org.gvsig.app.mainplugin/lib/postgresql-9.1-901.jdbc3.jar
+rm -f ${BASE_PATCHES_PATH}/gvSIG/extensiones/org.gvsig.app.mainplugin/lib/postgresql-9.1-901.jdbc3.jar
 cp portable/common/patches/postgresql-42.2.6.jar ${BASE_PATCHES_PATH}/gvSIG/extensiones/org.gvsig.app.mainplugin/lib/postgresql-42.2.6.jar
--- src/main/java/es/icarto/gvsig/sixhiara/forms/FontesCaracHidroSubForm.java
+++ src/main/java/es/icarto/gvsig/sixhiara/forms/FontesCaracHidroSubForm.java
@@ -6,7 +6,7 @@ import es.icarto.gvsig.navtableforms.gui.tables.AbstractSubForm;
 public class FontesCaracHidroSubForm extends AbstractSubForm {
 
 	public static final String TABLENAME = "fontes_carac_hidro";
-	public static String[] colNames = { "cadastro", "data_hid", "tipo_cama",
+	public static String[] colNames = { "cadastro", "data_lit", "tipo_cama",
 			"tipo_aqui", "q_aquifer", "conductiv" };
 	public static String[] colAlias = { "Cadastro", "Data ensaio", "Tipo Camada",
 			"Tipo Aquífero", "Caudal(m3/s)", "Conductividade(µS/cm)" };
