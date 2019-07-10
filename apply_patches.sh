e0c86126fe3a8481cc3c7adc7dcf0beb50b57c86
Francisco Puga <fpuga@icarto.es>
2019-07-10 19:02:11 +0200

    Adds new field "tipo_aqui" to "fontes_analise"
    refs #1689

--- apply_patches.sh
+++ apply_patches.sh
@@ -16,4 +16,5 @@ cp portable/common/patches/org.gvsig.gdal.prov.ogr-1.0.32.jar ${BASE_PATCHES_PAT
 cp portable/common/patches/org.gvsig.datalocator.app.mainplugin-2.0.157.jar ${BASE_PATCHES_PATH}/gvSIG/extensiones/org.gvsig.datalocator.app.mainplugin/lib/org.gvsig.datalocator.app.mainplugin-2.0.157.jar
 cp portable/common/patches/org.gvsig.fmap.dal.db.jdbc-2.0.157.jar ${BASE_PATCHES_PATH}/gvSIG/extensiones/org.gvsig.app.mainplugin/lib/
 rm -f ${BASE_PATCHES_PATH}/gvSIG/extensiones/org.gvsig.app.mainplugin/lib/postgresql-9.1-901.jdbc3.jar
-cp portable/common/patches/postgresql-42.2.6.jar ${BASE_PATCHES_PATH}/gvSIG/extensiones/org.gvsig.app.mainplugin/lib/postgresql-42.2.6.jar
+rm -f ${BASE_PATCHES_PATH}/gvSIG/extensiones/org.gvsig.postgresql.app.mainplugin/lib/postgresql-9.1-901.jdbc3.jar
+cp portable/common/patches/postgresql-42.2.6.jar ${BASE_PATCHES_PATH}/gvSIG/extensiones/org.gvsig.postgresql.app.mainplugin/lib/postgresql-42.2.6.jar
