2e272be9025eae956e28ff78a0ed5c35863be61c
Francisco Puga <fpuga@icarto.es>
2019-10-08 13:43:37 +0200

    Adds method isViewActive to AbstractExtension

--- src/main/java/es/icarto/gvsig/sixhiara/LoadGPXExtension.java
+++ src/main/java/es/icarto/gvsig/sixhiara/LoadGPXExtension.java
@@ -21,7 +21,7 @@ import es.icarto.gvsig.commons.AbstractExtension;
 import es.icarto.gvsig.commons.datasources.GPXFactory;
 
 public class LoadGPXExtension extends AbstractExtension {
-	
+
 	private static final Logger logger = LoggerFactory
 			.getLogger(LoadGPXExtension.class);
 
@@ -30,33 +30,35 @@ public class LoadGPXExtension extends AbstractExtension {
 	@Override
 	public void execute(String actionCommand) {
 		String homeFolder = System.getProperty("user.home");
-		JFileChooser fileChooser = new JFileChooser(getClass().getName(), homeFolder);
+		JFileChooser fileChooser = new JFileChooser(getClass().getName(),
+				homeFolder);
 		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-		FileFilter gpxFilter = new FileNameExtensionFilter(_("gps_files"), "gpx");
+		FileFilter gpxFilter = new FileNameExtensionFilter(_("gps_files"),
+				"gpx");
 		fileChooser.setFileFilter(gpxFilter);
 		Component parent = (Component) PluginServices.getMainFrame();
-		if (fileChooser.showDialog(parent, _("load"))  == JFileChooser.APPROVE_OPTION) {
+		if (fileChooser.showDialog(parent, _("load")) == JFileChooser.APPROVE_OPTION) {
 			File selectedFile = fileChooser.getSelectedFile();
 			if (fileChooser.accept(selectedFile)) {
 				loadGPX(selectedFile);
 			}
 		}
 	}
-	
+
 	private void loadGPX(File selectedFile) {
 		try {
-			FLyrVect f = GPXFactory.getWaypointLyrFromGPX(selectedFile, "EPSG:4326");
+			FLyrVect f = GPXFactory.getWaypointLyrFromGPX(selectedFile,
+					"EPSG:4326");
 			FLayers layers = view.getMapControl().getMapContext().getLayers();
 			layers.add(f);
 		} catch (LoadLayerException e) {
 			logger.error(e.getMessage(), e);
 		}
 	}
-	
+
 	@Override
 	public boolean isEnabled() {
-		view = getView();
-		return view != null;
+		return isViewActive();
 	}
 
 }
