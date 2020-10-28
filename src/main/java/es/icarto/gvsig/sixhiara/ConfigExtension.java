b117cc91e0462d5bd1ae1f53dd6a5e3c6df170dc
Francisco Puga <fpuga@icarto.es>
2020-10-28 13:46:25 +0100

    Avoids open a form to validate features after inserting a geometry
    refs #2011

--- src/main/java/es/icarto/gvsig/sixhiara/ConfigExtension.java
+++ src/main/java/es/icarto/gvsig/sixhiara/ConfigExtension.java
@@ -4,6 +4,11 @@ import java.io.File;
 
 import org.gvsig.andami.Launcher;
 import org.gvsig.andami.plugins.Extension;
+import org.gvsig.fmap.dal.EditingNotification;
+import org.gvsig.fmap.dal.EditingNotificationManager;
+import org.gvsig.fmap.dal.swing.DALSwingLocator;
+import org.gvsig.tools.observer.Observable;
+import org.gvsig.tools.observer.Observer;
 
 import es.icarto.gvsig.sixhiara.forms.SixhiaraFormFactory;
 import es.udc.cartolab.gvsig.tools.CopyFeaturesExtension;
@@ -11,6 +16,7 @@ import es.udc.cartolab.gvsig.tools.CopyFeaturesExtension;
 public class ConfigExtension extends Extension {
 
 	public static final String NAME = "SIXHIARA";
+	private Observer observer;
 
 	@Override
 	public void initialize() {
@@ -18,6 +24,22 @@ public class ConfigExtension extends Extension {
 		String defaultPath = Launcher.getAppHomeDir() + File.separator
 				+ "importacao-gps";
 		CopyFeaturesExtension.setDefaultPath(defaultPath);
+		skipFeatureValidation();
+	}
+	
+	private void skipFeatureValidation() {
+		EditingNotificationManager editingNotificationManager = DALSwingLocator.getEditingNotificationManager();
+		  observer = new Observer() {
+		      @Override
+		      public void update(Observable observable, Object notification) {
+		          EditingNotification n = (EditingNotification) notification;
+		          n.setSkipFeatureValidation(true);
+//		          if (n.isOfType(EditingNotification.AFTER_INSERT_FEATURE)) {
+//		              Abrir formulario navtable
+//		          }
+		      }
+		  };
+		  editingNotificationManager.addObserver(observer);
 	}
 
 	@Override
